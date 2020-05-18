3044347a2fcf7d38de672ea1b4167f4fd4bf0997
Marty Kane <marty.kane@cmegroup.com>
2020-05-18 16:34:10 -0500

    Read client id and secret from config.json

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-
+/**
+ * This is "Main" where all of the modules are imported, configured, and tied together and the app is started.
+ */
 //const accessLogger = require('./accessLogger');
-
-//Note, the value of redirectUrl must match exactly with a value provided in the Patreon app web form
-const port = 80;
-const oauthRedirectPath = '/oauth/redirect';
-const redirectUrl = `http://localhost:${port}${oauthRedirectPath}`;
-
-//TODO: Read these values from environment or a config file (DON'T PUSH TO GITHUB WITH THIS REPO!)
-//const clientId = process.env.PATREON_CLIENT_ID
-const clientId = '7wL7w6W0MNfn98UUbTlY4daPYTdSrRMv657JUlnIseDP29iLCJ8XvDtZrTR-DOWG';
-//const clientSecret = process.env.PATREON_CLIENT_SECRET
-const clientSecret = 'cgQe0S-xT9aqi0iK2DsZzQiTBGefHO_OQaOkGKEMwrsVQQ-U6w8JiJo5f5jUC4UL';
-
 const PatreonApi = require('./src/PatreonApiInterface');
 const fetchCommsModule = require('./src/fetchCommsApiModule');
-const patreonApi = PatreonApi(clientId, clientSecret, redirectUrl, fetchCommsModule);
-
-const dataStore = require('./src/InMemoryDataStore')();
+const DataStore = require('./src/InMemoryDataStore');
 
 const server = require('./src/Server');
+
+const port = 80;
+
+const config = require('./config.json');
+const clientId = config.clientId || "";
+const clientSecret = config.clientSecret || "";
+
+const oauthRedirectPath = '/oauth/redirect';
+//Note, the value of redirectUrl must match exactly with a value provided in the Patreon app web form
+const redirectUrl = `http://localhost:${port}${oauthRedirectPath}`;
+
+const patreonApi = PatreonApi(clientId, clientSecret, redirectUrl, fetchCommsModule);
+const dataStore = DataStore();
+
 server.initialize(port, oauthRedirectPath, patreonApi, dataStore);
